ed2b25b0780a41e9d0ee685ab59b5f11ce0531c9
Micah J <justmic007>
2024-08-31 17:57:55 +0100

    auth service project setup

---
+++ src/config.ts
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+
+dotenv.config({});
+
+class Config {
+  public NODE_ENV: string | undefined;
+  public CLIENT_URL: string | undefined;
+  public GATEWAY_JWT_TOKEN: string | undefined;
+  public JWT_TOKEN: string | undefined;
+  public API_GATEWAY_URL: string | undefined;
+  public MYSQL_DB: string | undefined;
+  public CLOUD_NAME: string | undefined;
+  public CLOUD_API_KEY: string | undefined;
+  public CLOUD_API_SECRET: string | undefined;
+  public RABBITMQ_ENDPOINT: string | undefined;
+  public ELASTIC_SEARCH_URL: string | undefined;
+  public ELASTIC_APM_SERVER_URL: string | undefined;
+  public ELASTIC_APM_SECRET_TOKEN: string | undefined;
+
+  constructor(){
+    this.NODE_ENV = process.env.NODE_ENV || '';
+    this.CLIENT_URL = process.env.CLIENT_URL || '';
+    this.GATEWAY_JWT_TOKEN = process.env.GATEWAY_JWT_TOKEN || '';
+    this.JWT_TOKEN = process.env.JWT_TOKEN || '';
+    this.API_GATEWAY_URL = process.env.API_GATEWAY_URL || '';
+    this.MYSQL_DB = process.env.MYSQL_DB || '';
+    this.CLOUD_NAME = process.env.CLOUD_NAME || '';
+    this.CLOUD_API_KEY = process.env.CLOUD_API_KEY || '';
+    this.CLOUD_API_SECRET = process.env.CLOUD_API_SECRET || '';
+    this.RABBITMQ_ENDPOINT = process.env.RABBITMQ_ENDPOINT || '';
+    this.ELASTIC_SEARCH_URL = process.env.ELASTIC_SEARCH_URL || '';
+    this.ELASTIC_APM_SERVER_URL = process.env.ELASTIC_APM_SERVER_URL || '';
+    this.ELASTIC_APM_SECRET_TOKEN = process.env.ELASTIC_APM_SECRET_TOKEN || '';
+  }
+}
+
+export const config: Config = new Config;
